b50487b7cdec9229f84e5d03248f6450716718a8
mjhenning <mjhenning2003@gmail.com>
2026-05-12 13:09:45 +0200

    Rethink of structure

---
+++ Data/ARGData.cs
@@ -0,0 +1,18 @@
+namespace DiscordBot.Data;
+
+public class ArgTerminalSession
+{
+    public string UIMessageID { get; set; }
+    public string ContentMessageID { get; set; }
+    public string Cwd { get; set; }
+    public string LastAction { get; set; }
+    
+    int activeUsers {get; set;}
+    
+    public List<string> ActionHistory { get; set; }
+}
+
+public class ARGReadSession
+{
+    
+}
--- Modules/ARG.cs
+++ Modules/ARG.cs
@@ -2,29 +2,7 @@ namespace DiscordBot.Modules;
 
 public class ARG
 {
-    Dictionary<FolderAndFiles, FolderAndFiles> filesystem = new Dictionary<FolderAndFiles, FolderAndFiles>();
 
-    public void PopulateFilesystem()
-    {
-        string[] baseDirectories = Directory.GetDirectories(Config.ARGDirectory);
-        
-        for (int i = 0; i < baseDirectories.Length; i++)
-        {
-            //if basedirectory contains directories do x
-            
-            //else if contains no directories
-            
-            // filesystem.Add(baseDirectories[i]., Directory.GetDirectories(Config.ARGDirectory + "/" + baseDirectories[i]));
-        }
-    }
     
     
-}
-
-class FolderAndFiles()
-{
-    public bool containsSubFolders = false; //if false assume contains files OR root folder
-    public string folder;
-    public List<string> subfolders;
-    public List<string> files;
 }
