82e4a792dbc282b4b7b586e6224c953ef39a64f9
mjhenning <mjhenning2003@gmail.com>
2026-08-24 23:14:55 +0200

    Spelling mistake in favourites list

--- Modules/Twitch_Automation/FavouritesLiveNoti.cs
+++ Modules/Twitch_Automation/FavouritesLiveNoti.cs
@@ -26,7 +26,7 @@ public class FavouritesLiveNoti
         { "InnocentOfSin", "Definitely not a cult, but brother can this owl yap! 🧡 Go checkout the amazing sin and his sussy but lovely community!"},
         { "BaxxyCH", "Go catch our lovely family from next door, the baxxidents!!! 💜 Make sure to keep up with their chaotic energy on {game}!"},
         { "LaeliaTheCat", "Fox's favourite chef star kitty is live with {game}!!! 🌟 Make sure to go pop in and say hi!!"},
-        { "Juliuskat", "Our Finish Feline from next door is live with {game}! Go show the katpack some love! 🤍"},
+        { "Juliuskat", "Our Finnish Feline from next door is live with {game}! Go show the katpack some love! 🤍"},
         { "violenciakurayami", "Fox's favourite sharkie is busy with {game}, go say hi to our bubbly family, the fishies! 🫧"},
         { "pathetic_softpaw", "You love art right?! Go check out fox's favourite vibe artist, paw paw! Maybe she's up to {game} and not art this time? 🩷"},
         {"Silbers_", "Scug?! The only one I know is Sticky! Go check out this amazing scug and her community (maybe drop a wawa in chat) whether it's {game} or something else!🩶"}
